d520829ba5738d722279ee6459a9e49ad5aafdd6
Fatma Janna <fatma.janna@quinyx.com>
2023-05-29 20:21:55 +0700

    update password length to store encrypted password

--- backend/app/main/models.py
+++ backend/app/main/models.py
@@ -7,8 +7,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f"<User {self.username}>"
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "created_at": self.created_at
+        }
