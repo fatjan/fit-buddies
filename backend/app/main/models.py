390061b30f44521acd765cb2cacd193401b2a400
Fatma Janna <fatma.janna@quinyx.com>
2023-05-29 19:53:54 +0700

    rearrange user model

--- backend/app/main/models.py
+++ backend/app/main/models.py
@@ -1,8 +1,14 @@
-from app import db
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return f'<User {self.name}>'
+        return f"<User {self.username}>"
